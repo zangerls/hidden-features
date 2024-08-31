122f42f4dd3710eae6969372e0cabe754f96b0e4
Simon Zangerl <simon.zangerl@hotmail.com>
2024-08-31 20:31:36 +0200

    Decided to use Tailwind for styling...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Poppins} from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({weight: ["400", "600", "700"], });
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600", "700"] });
 
 export const metadata: Metadata = {
   title: "Title",
