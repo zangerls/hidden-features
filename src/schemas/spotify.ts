aa1775e74625b4a6c4d4001f63c56c6e091d5e4e
Simon Zangerl <simon.zangerl@hotmail.com>
2024-09-01 15:22:14 +0200

    Added additional Spotify types & schemas

--- src/schemas/spotify.ts
+++ src/schemas/spotify.ts
@@ -22,3 +22,51 @@ export const artistsDataSchema = z.object({
     items: z.array(artistSchema),
   }),
 });
+
+export const albumSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  album_type: z.union([
+    z.literal("album"),
+    z.literal("single"),
+    z.literal("compilation"),
+  ]),
+  total_tracks: z.number().int().nonnegative(),
+  images: z.array(imageSchema),
+  release_date: z.string(),
+  release_date_precision: z.union([
+    z.literal("year"),
+    z.literal("month"),
+    z.literal("day"),
+  ]),
+});
+
+export const simplifiedAlbumSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+});
+
+export const albumsDataSchema = z.object({
+  next: z.nullable(z.string()),
+  total: z.number().int().nonnegative(),
+  items: z.array(albumSchema),
+});
+
+export const simplifiedArtistSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+});
+
+export const trackSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  artists: z.array(simplifiedArtistSchema),
+  duration_ms: z.number().int().nonnegative(),
+  track_number: z.number().int().nonnegative(),
+});
+
+export const tracksDataSchema = z.object({
+  next: z.nullable(z.string()),
+  total: z.number().int().nonnegative(),
+  items: z.array(trackSchema),
+});
--- src/types/spotify.d.ts
+++ src/types/spotify.d.ts
@@ -1,4 +1,18 @@
-import { artistSchema } from "@/schemas/spotify";
+import {
+  albumSchema,
+  artistSchema,
+  simplifiedAlbumSchema,
+  simplifiedArtistSchema,
+  trackSchema,
+} from "@/schemas/spotify";
 import { z } from "zod";
 
 export type Artist = z.infer<typeof artistSchema>;
+
+export type Album = z.infer<typeof albumSchema>;
+
+export type Track = z.infer<typeof trackSchema>;
+
+export type SimplifiedArtist = z.infer<typeof simplifiedArtistSchema>;
+
+export type SimplifiedAlbum = z.infer<typeof simplifiedAlbumSchema>;
